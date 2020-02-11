e3250d4203968f10d7d394f6711108626263900b
tianyul <tianyul@xilinx.com>
2020-02-11 14:45:33 -0800

    modify json format & docker build and push

--- nimbixlize.py
+++ nimbixlize.py
@@ -6,38 +6,13 @@ import os
 import datetime
 import shutil 
 
-def list_machines() :
-    print "Available Machines Types on Nimbix:"
-    print ("")
-    print ("Machine Type ID         Platform                      Mark")
-    print ("n2                      N/A                           8 core, 64GB RAM (CPU only)")
-    print ("n3                      N/A                           16 core, 128GB RAM (CPU only)")
-    print ("n4                      N/A                           16 core, 256GB RAM (CPU only)")
-    print ("n5                      N/A                           16 core, 512GB RAM (CPU only)")
-    print ("n9                      N/A                           20 core Intel Skylake, 192GB RAM (CPU only)")
-    print ("nx5u                    xilinx_u200_xdma_201820_1     16 core, 128GB RAM, Xilinx Alveo U200 FPGA 2018.2 XDF")
-    print ("nx5u_xdma_201830_1      xilinx_u200_xdma_201830_1     16 core, 128GB RAM, Xilinx Alveo U200 FPGA 2018.3")
-    print ("nx5u_xdma_201830_2      xilinx_u200_xdma_201830_2     16 core, 128GB RAM, Xilinx Alveo U200 FPGA 2019.1")
-    print ("nx5u_xdma_201830_2_2_3  xilinx_u200_xdma_201830_2     16 core, 128GB RAM, Xilinx Alveo U200 FPGA 2019.2")
-    print ("nx6u                    xilinx_u250_xdma_201820_1     16 core, 128GB RAM, Xilinx Alveo U250 FPGA 2018.2 XDF")
-    print ("nx6u_xdma_201830_1      xilinx_u250_xdma_201830_1     16 core, 128GB RAM, Xilinx Alveo U250 FPGA 2018.3")
-    print ("nx6u_xdma_201830_2      xilinx_u250_xdma_201830_2     16 core, 128GB RAM, Xilinx Alveo U250 FPGA 2019.1")
-    print ("nx6u_xdma_201830_2_2_3  xilinx_u250_xdma_201830_2     16 core, 128GB RAM, Xilinx Alveo U250 FPGA 2019.2")
-    print ("nx7u_xdma_201920_1      xilinx_u280_xdma_201920_1     16 core, 128GB RAM, Xilinx Alveo U280 FPGA 2019.2")
-
 def list_tags() :
     print ("Available platform and XRT combination:")
     print ("")
-    print ("Platform                                  XRT Version    OS Version")
-    print ("alveo-u200 / alveo-u250                   2018.3         CentOS")
-    print ("alveo-u200 / alveo-u250                   2018.3         Ubuntu 16.04")
-    print ("alveo-u200 / alveo-u250                   2018.3         Ubuntu 18.04")
-    print ("alveo-u200 / alveo-u250 / alveo-u280      2019.1         CentOS")
-    print ("alveo-u200 / alveo-u250 / alveo-u280      2019.1         Ubuntu 16.04")
-    print ("alveo-u200 / alveo-u250 / alveo-u280      2019.1         Ubuntu 18.04")
-    print ("alveo-u200 / alveo-u250 / alveo-u280      2019.2         CentOS")
-    print ("alveo-u200 / alveo-u250 / alveo-u280      2019.2         Ubuntu 16.04")
-    print ("alveo-u200 / alveo-u250 / alveo-u280      2019.2         Ubuntu 18.04")
+    print ("Platform       XRT Version                  OS Version")
+    print ("alveo-u200     2018.3 /2019.1 / 2019.2      Ubuntu 16.04 / Ubuntu 18.04 / CentOS")
+    print ("alveo-u250     2018.3 /2019.1 / 2019.2      Ubuntu 16.04 / Ubuntu 18.04 / CentOS")
+    print ("alveo-u280     2019.2                       Ubuntu 16.04 / Ubuntu 18.04 / CentOS")
 
 with open('config.json') as d:
     repos = json.load(d)
@@ -66,13 +41,12 @@ if not app_info['platform']:
     print("Platform can NOT be empty!")
     exit(1)
 
-if post_processors['push_after_build']:
-    if not post_processors['repository']:
-        print("Repository can NOT be empty!")
-        exit(1)
-    if not post_processors['tag']:
-        print("Tag can NOT be empty!")
-        exit(1)
+if not post_processors['repository']:
+    print("Repository can NOT be empty!")
+    exit(1)
+if not post_processors['tag']:
+    print("Tag can NOT be empty!")
+    exit(1)
 
 with open('spec.json') as d:
     spec = json.load(d)
@@ -102,10 +76,10 @@ except OSError:
     exit(1)
 
 commands = []
-copies = []
+
 for pro in provisioners:
     ctype = pro['type']
-    if ctype == 'cmd':
+    if ctype == 'shell':
         commands.append("RUN " + " & ".join(pro['inline']))
     elif ctype == 'file':
         if not os.path.exists(pro['source']):
@@ -113,15 +87,15 @@ for pro in provisioners:
             exit(1)
         shutil.copy(pro['source'], path + "/")
         filename = os.path.basename(pro['source'])
-        copies.append("COPY " + filename + " " + pro['destination'])
+        commands.append("COPY " + filename + " " + pro['destination'])
+    else:
+        print("Warning: Unknown type: " + ctype + "! ")
 
 with open("Dockerfile.example", "r") as f:
     s = f.read()
     s = s.replace("__from_image__", image_url)
     with open(path + "/Dockerfile", "w") as d:
         d.write(s)
-        for copy in copies:
-            d.write(copy+"\n")
         for command in commands:
             d.write(command + "\n")
 
@@ -137,13 +111,17 @@ if target_platform not in appdef['machines']:
 
 
 with open(path + '/AppDef.json', "w") as d:
-    json.dump(appdef, d)
+    json.dump(appdef, d, indent=4)
 
 #Build application
 subprocess.check_output(
-    "docker build -t " + post_processors['repository'] + ":" + post_processors["tag"] + " -f " + path + "/Dockerfile",
-    stderr=subprocess.STDOUT)
+    "docker build -t " + post_processors['repository'] + ":" + post_processors["tag"] + " " + path,
+    stderr=subprocess.STDOUT, shell=True)
 
 if post_processors['push_after_build']:
-    subprocess.check_output("docekr push " + post_processors['repository'] + ":" + post_processors["tag"],
-    stderr=subprocess.STDOUT)
+    print("docker push " + post_processors['repository'] + ":" + post_processors["tag"])
+    subprocess.check_output("docker push " + post_processors['repository'] + ":" + post_processors["tag"],
+    stderr=subprocess.STDOUT, shell=True)
+
+print("Build successfully!")
+exit(0)
